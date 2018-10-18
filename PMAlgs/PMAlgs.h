4d2caa7b468b147c94025efe9674b70e423be1e4
pomalone91 <pomalone91@gmail.com>
2018-10-17 19:45:52 -0600

    Initial Commit

---
+++ PMAlgs/PMAlgs.h
@@ -0,0 +1,19 @@
+//
+//  PMAlgs.h
+//  PMAlgs
+//
+//  Created by Paul Malone on 10/17/18.
+//  Copyright © 2018 Paul Malone. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PMAlgs.
+FOUNDATION_EXPORT double PMAlgsVersionNumber;
+
+//! Project version string for PMAlgs.
+FOUNDATION_EXPORT const unsigned char PMAlgsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMAlgs/PublicHeader.h>
+
+
